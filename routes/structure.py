49acc06b239405cc22bb70e591137418c20446cc
Sony Alamsyah M <sonyalamsyah.learn@gmail.com>
2020-10-22 17:25:11 +0700

    add temporary user structure

---
+++ routes/structure.py
@@ -0,0 +1,11 @@
+user_structure = {
+    "user_id": "",
+    "username": "",
+    "password": "",
+    "email": "",
+    "created_at": 0,
+    "created_by": "",
+    "is_banned": False,
+    "is_active": False,
+    "last_login": 0
+}
